c146220d6a1e584871978e4b33057e9e84153006
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-08-14 00:02:07 -0300

    feat: adicionando database models de user

---
+++ app/models/database_models/__init__.py
@@ -0,0 +1,1 @@
+from app.infrastructure.database_engine import Base
---
+++ app/models/database_models/user.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from app.infrastructure.database_engine import Base
+import uuid
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True,
+                default=lambda: str(uuid.uuid4()))
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
