1fd15596399cb0c98b3a58eec85c6a726c17e32b
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-08-18 18:37:26 -0300

    feat: adição de execeções de usuário

---
+++ app/exceptions/user_exceptions.py
@@ -0,0 +1,16 @@
+from fastapi import HTTPException, status
+
+
+class UserAlreadyExistsException(HTTPException):
+    def __init__(self, detail: str = "Username or email already exists"):
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+class UserPasswordException(HTTPException):
+    def __init__(self, detail: str = "Password exception"):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
+        
+        
+class UserEmailException(HTTPException):
+    def __init__(self, detail: str = "Email exception"):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
