c64bf8bf70c07c16ecd034e63f1207a3df365e65
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-08-18 10:31:32 -0300

    feat: configuraçãoes adicionais

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -8,6 +8,13 @@ class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL") or "sqlite:///./sql_app.db"
     DATABASE_TEST: bool = (os.getenv("DATABASE_TEST")
                            or "False").lower() == "true"
+    DATABASE_TEST_URL = "sqlite:///./test.db"
+    ORIGINS: str = os.getenv(
+        "APP_CONFIG_ORIGINS") or '["http://localhost:3000"]'
+    METHODS: str = os.getenv("APP_CONFIG_METHODS") or '["*"]'
+    HEADERS: str = os.getenv("APP_CONFIG_HEADERS") or '["*"]'
+    CREDENTIALS: bool = (os.getenv("APP_CONFIG_CREDENTIALS")
+                         or "True").lower() == "true"
 
 
 settings = Settings()
