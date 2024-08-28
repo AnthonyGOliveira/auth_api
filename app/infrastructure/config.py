7c8472fe7da503f6281c6efd761b807f22da6fe4
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-08-27 21:27:15 -0300

    chore: novas configs de ambiente

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -15,6 +15,12 @@ class Settings:
     HEADERS: str = os.getenv("APP_CONFIG_HEADERS") or '["*"]'
     CREDENTIALS: bool = (os.getenv("APP_CONFIG_CREDENTIALS")
                          or "True").lower() == "true"
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY") or "52596af96fd3b11c4bcb0181e8050607131dfb9f01ef52309603c65e0103e141"
+    ALGORITHM = os.getenv("ALGORITHM") or "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES") or 30)
+    SERVER_HOST = os.getenv("SERVER_HOST") or "example.com"
 
 
 settings = Settings()
